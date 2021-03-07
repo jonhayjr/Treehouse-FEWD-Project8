703e2cef1651e6f19473f086f8214a0844f12575
jonhayjr <jonhayjr@gmail.com>
2021-03-07 09:36:55 -0800

    Updated date and phone formatting.  Created custom function to find state abbreviation

--- js/app.js
+++ js/app.js
@@ -49,6 +49,12 @@ function displayModal(index) {
     let { name, dob, phone, email, location: { city, street, state, postcode
     }, picture } = employees[index];
     let date = new Date(dob.date);
+    let dateMonth = date.getMonth().toString().padStart(2, "0");
+    let dateDay = date.getDate().toString().padStart(2, "0");
+    let dateYear = date.getFullYear().toString().substr(-2);
+    let phoneFormatted = phone.toString().replace('-', ' ');
+    let stateAbbreviation = findStateAbbreviation(state);
+
     const modalHTML = `
     <img class="avatar" src="${picture.large}" />
     <div class="text-container" data-index="${index}">
@@ -56,14 +62,87 @@ function displayModal(index) {
     <p class="email">${email}</p>
     <p class="address">${city}</p>
     <hr />
-    <p class="phone">${phone}</p>
-    <p class="street-address">${street.number} ${street.name}, ${state} ${postcode}</p>
+    <p class="phone">${phoneFormatted}</p>
+    <p class="street-address">${street.number} ${street.name}, ${stateAbbreviation} ${postcode}</p>
     <p class="birthday">Birthday:
-    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    ${dateMonth}/${dateDay}/${dateYear}</p>
     </div>
     `;
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
+}
+
+/*Custom Function to find state abbreviation*/
+function findStateAbbreviation(state) {
+    const usStates = [
+        { name: 'ALABAMA', abbreviation: 'AL'},
+        { name: 'ALASKA', abbreviation: 'AK'},
+        { name: 'AMERICAN SAMOA', abbreviation: 'AS'},
+        { name: 'ARIZONA', abbreviation: 'AZ'},
+        { name: 'ARKANSAS', abbreviation: 'AR'},
+        { name: 'CALIFORNIA', abbreviation: 'CA'},
+        { name: 'COLORADO', abbreviation: 'CO'},
+        { name: 'CONNECTICUT', abbreviation: 'CT'},
+        { name: 'DELAWARE', abbreviation: 'DE'},
+        { name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC'},
+        { name: 'FEDERATED STATES OF MICRONESIA', abbreviation: 'FM'},
+        { name: 'FLORIDA', abbreviation: 'FL'},
+        { name: 'GEORGIA', abbreviation: 'GA'},
+        { name: 'GUAM', abbreviation: 'GU'},
+        { name: 'HAWAII', abbreviation: 'HI'},
+        { name: 'IDAHO', abbreviation: 'ID'},
+        { name: 'ILLINOIS', abbreviation: 'IL'},
+        { name: 'INDIANA', abbreviation: 'IN'},
+        { name: 'IOWA', abbreviation: 'IA'},
+        { name: 'KANSAS', abbreviation: 'KS'},
+        { name: 'KENTUCKY', abbreviation: 'KY'},
+        { name: 'LOUISIANA', abbreviation: 'LA'},
+        { name: 'MAINE', abbreviation: 'ME'},
+        { name: 'MARSHALL ISLANDS', abbreviation: 'MH'},
+        { name: 'MARYLAND', abbreviation: 'MD'},
+        { name: 'MASSACHUSETTS', abbreviation: 'MA'},
+        { name: 'MICHIGAN', abbreviation: 'MI'},
+        { name: 'MINNESOTA', abbreviation: 'MN'},
+        { name: 'MISSISSIPPI', abbreviation: 'MS'},
+        { name: 'MISSOURI', abbreviation: 'MO'},
+        { name: 'MONTANA', abbreviation: 'MT'},
+        { name: 'NEBRASKA', abbreviation: 'NE'},
+        { name: 'NEVADA', abbreviation: 'NV'},
+        { name: 'NEW HAMPSHIRE', abbreviation: 'NH'},
+        { name: 'NEW JERSEY', abbreviation: 'NJ'},
+        { name: 'NEW MEXICO', abbreviation: 'NM'},
+        { name: 'NEW YORK', abbreviation: 'NY'},
+        { name: 'NORTH CAROLINA', abbreviation: 'NC'},
+        { name: 'NORTH DAKOTA', abbreviation: 'ND'},
+        { name: 'NORTHERN MARIANA ISLANDS', abbreviation: 'MP'},
+        { name: 'OHIO', abbreviation: 'OH'},
+        { name: 'OKLAHOMA', abbreviation: 'OK'},
+        { name: 'OREGON', abbreviation: 'OR'},
+        { name: 'PALAU', abbreviation: 'PW'},
+        { name: 'PENNSYLVANIA', abbreviation: 'PA'},
+        { name: 'PUERTO RICO', abbreviation: 'PR'},
+        { name: 'RHODE ISLAND', abbreviation: 'RI'},
+        { name: 'SOUTH CAROLINA', abbreviation: 'SC'},
+        { name: 'SOUTH DAKOTA', abbreviation: 'SD'},
+        { name: 'TENNESSEE', abbreviation: 'TN'},
+        { name: 'TEXAS', abbreviation: 'TX'},
+        { name: 'UTAH', abbreviation: 'UT'},
+        { name: 'VERMONT', abbreviation: 'VT'},
+        { name: 'VIRGIN ISLANDS', abbreviation: 'VI'},
+        { name: 'VIRGINIA', abbreviation: 'VA'},
+        { name: 'WASHINGTON', abbreviation: 'WA'},
+        { name: 'WEST VIRGINIA', abbreviation: 'WV'},
+        { name: 'WISCONSIN', abbreviation: 'WI'},
+        { name: 'WYOMING', abbreviation: 'WY' }
+    ]
+
+    const index = usStates.findIndex(usState => usState.name === state.toUpperCase());
+
+    if (index < 0) {
+        return state;
+    } else {
+        return usStates[index].abbreviation;
+    }
 }
 
 gridContainer.addEventListener('click', e => {
